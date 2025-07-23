68b95a0553bab21eadc2b3827cd3d3479f877779
Naji4054 <mhdnaji4054@gmail.com>
2025-07-23 10:56:29 +0530

    learning javascript

---
+++ class.js
@@ -0,0 +1,91 @@
+
+
+// const arr = [
+//     {
+//         name: 'sarath',
+//         year: 1,
+//         age: 11
+//     },
+//     {
+//         name: 'adi',
+//         year: 2,
+//         age: 13
+//     },
+//     {
+//         name: 'naji',
+//         year: 3,
+//         age: 11
+//     },
+//     {
+//         name: 'rai',
+//         year: 1,
+//         age: 11
+//     },
+//     {
+//         name: 'vai',
+//         year: 4,
+//         age: 13
+//     },
+//     {
+//         name: 'vish',
+//         year: 5,
+//         age: 11
+//     },
+// ]
+
+
+// const newArr = arr.find((item)=> item.age === 13)
+
+
+// console.log(newArr)
+
+
+// const obj = {
+//     name: 'adi',
+//     age: 21,
+//     year: 2021
+// }
+
+// // Object.keys(obj).map((item)=> {
+// //     console.log(item, "  :   ", obj[item])
+// // })
+
+
+
+// console.log(obj[year])
+
+
+class Maitexa {
+    constructor(a, v) {
+       this.name = a
+       this.course = v
+    }
+
+    details() {
+        console.log(`Hi ${this.name}`)
+    }
+}
+
+
+const adi = new Maitexa('adish', 'python')
+const vish = new Maitexa('vish', 'python')
+
+adi.details()
+vish.details()
+
+
+// const name = 'adish'
+// const course = 'python'
+// const name1 = 'vish'
+// const course1 = 'python'
+// const name2 = 'naji'
+// const course2 = 'python'
+
+// console.log(`Name: ${name} \nCourse: ${course}`)
+// console.log(`Name: ${name1} \nCourse: ${course1}`)
+// console.log(`Name: ${name2} \nCourse: ${course2}`)
+
+
+
+
+
---
+++ fetch.js
@@ -0,0 +1,25 @@
+
+
+
+// let productsArr = []
+
+// fetch('https://fakestoreapi.com/products').then(res=> res.json()).then((data)=> {
+//     console.log(data)
+
+//     listProducts(data)
+//  })
+
+ 
+ 
+//  const listProducts = (arr)=> {
+//     let htmlContent = ''
+//     arr.forEach((item)=> {
+//         htmlContent += `<div>${item.title}</div>`
+//     })
+
+//     const proCon = document.getElementById('proCon')
+//     proCon.innerHTML = htmlContent
+//  }
+
+
+ 
